d3e96295dbbb66695e3a80f97d194895bf1e0b0e
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-12-23 01:01:02 -0500

    add typescript to project

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './custom.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './custom.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
