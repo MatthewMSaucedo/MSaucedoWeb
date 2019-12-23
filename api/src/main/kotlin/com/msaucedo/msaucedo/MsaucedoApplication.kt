05e6a7bfe1e477e8cc821b6d6db542ae4eae2158
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-12-22 20:25:13 -0500

    intial commit

---
+++ api/src/main/kotlin/com/msaucedo/msaucedo/MsaucedoApplication.kt
@@ -0,0 +1,11 @@
+package com.msaucedo.msaucedo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaucedoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsaucedoApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/msaucedo/msaucedo/MsaucedoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msaucedo.msaucedo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaucedoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
